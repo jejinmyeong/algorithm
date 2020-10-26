2e7de97b78c370d7a485ac2b781bbf7e66d3f709
jejinmyeong <1995jjm@naver.com>
2020-10-27 06:14:01 +0900

    코딩테스트 연습

--- boj/9663.py
+++ boj/9663.py
@@ -12,3 +12,35 @@ output:
 첫째 줄에 퀸 N개를 서로 공격할 수 없게 놓는 경우의 수를 출력한다.
 """
 
+n = int(input())
+answer = 0
+
+def dfs(_list, add, n):
+	_list.append(add)
+
+	global answer
+	if len(_list) == n:
+		answer += 1
+		return 0
+
+
+	check = [n for n in range(n)]
+
+	for idx, i in enumerate(_list):
+		if i in check:
+			check.remove(i)
+
+		distance = len(_list) - idx
+
+		if i - distance in check:
+			check.remove(i - distance)
+		if i + distance in check:
+			check.remove(i + distance)
+
+	if check:
+		for idx, i in enumerate(check):
+			dfs([n for n in _list], i, n)
+
+for i in range(n):
+	dfs([], i, n)
+print(answer)
