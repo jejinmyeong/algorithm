0988c5d759df94737710678c53fa71b8a35b621c
jejinmyeong <1995jjm@naver.com>
2021-06-23 10:46:09 +0900

    DFS 순위

--- programmers/49191_dfs.py
+++ programmers/49191_dfs.py
@@ -1,26 +1,22 @@
 def solution(n, results):
     answer = 0
-
-    win_arr = [[0] *(n) for _ in range(n)]
+    win_table = [[0]*n for _ in range(n)]
     WIN, LOSE = 1, -1
 
     for w, l in results:
-        win_arr[w-1][l-1], win_arr[l-1][w-1] = WIN, LOSE
-
-    for i in range(n):
-        loses = [idx for idx, rst in enumerate(win_arr[i]) if rst == LOSE]
-        
-        while loses:
-            winer = loses.pop()
-            for idx, rst in enumerate(win_arr[winer]):
-                if rst == LOSE and win_arr[i][idx] == 0:
-                    win_arr[i][idx], win_arr[idx][i] = LOSE, WIN
-                    loses.append(idx)
-            for w in win_arr:
-                print(w)
-            print()
+        win_table[w-1][l-1], win_table[l-1][w-1] = WIN, LOSE
     
-    answer = len([x for x in win_arr if x.count(0) == 1])
+    for now in range(n):
+        wins = [idx for idx, rst in enumerate(win_table[now]) if rst == WIN]
+        while wins:
+            loser = wins.pop()
+            for idx, temp in enumerate(win_table[loser]):
+                if temp == WIN and win_table[now][idx] == 0:
+                    win_table[now][idx], win_table[idx][now] = WIN, LOSE
+                    wins.append(idx)
+    
+    answer = len(['ok' for rst in win_table if rst.count(0) == 1])
+
 
     return answer
 
@@ -32,3 +28,5 @@ def main():
 
 if __name__ == "__main__":
     main()
+
+
