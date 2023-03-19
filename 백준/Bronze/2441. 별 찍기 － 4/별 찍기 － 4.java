f1b27b6dfa22309e2d5e866304981ae7be1c43bb
jejinmyeong <1995jjm@naver.com>
2023-03-19 23:20:19 +0900

    [Bronze III] Title: 별 찍기 - 4, Time: 80 ms, Memory: 11628 KB -BaekjoonHub

---
+++ 백준/Bronze/2441. 별 찍기 － 4/별 찍기 － 4.java
@@ -0,0 +1,21 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        
+        int N = Integer.parseInt(br.readLine());
+        
+        for (int i = 0 ; i < N ; i++) {
+            for (int j = 0 ; j < N ; j++) {
+                if (i > j) sb.append(" ");
+                else sb.append("*");
+            }
+            sb.append("\n");
+        }
+        
+        System.out.print(sb);
+    }
+}
