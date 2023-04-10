ec50aa3be1b2fa9e02e1cd378af49861314d63e7
jejinmyeong <1995jjm@naver.com>
2023-04-10 22:36:56 +0900

    [Gold V] Title: a^b, Time: 80 ms, Memory: 11856 KB -BaekjoonHub

---
+++ 백준/Gold/10827. a＾b/a＾b.java
@@ -0,0 +1,15 @@
+import java.math.*;
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        BigDecimal a = new BigDecimal(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+
+        System.out.println(a.pow(b).toPlainString());
+    }
+}
