0b8a58f43304e1e23c7ec12159cc13295fa287a4
jejinmyeong <1995jjm@naver.com>
2023-04-14 15:09:11 +0900

    [unrated] Title: 점 찍기, Time: 0.08 ms, Memory: 68 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/140107. 점 찍기/점 찍기.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+class Solution {
+    public long solution(int k, int d) {
+        long answer = 0;
+        
+        for (long i = 0 ; i <= d ; i += k) {
+            answer += ((long)Math.sqrt((long)d*d - i*i)) / k + 1;
+        }
+        return answer;
+    }
+}
