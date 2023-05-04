3da19986d7710d98d01a1aa45805eb92c3529985
jejinmyeong <1995jjm@naver.com>
2023-05-04 13:47:20 +0900

    [Gold V] Title: 합분해, Time: 76 ms, Memory: 11588 KB -BaekjoonHub

---
+++ 백준/Gold/2225. 합분해/합분해.java
@@ -0,0 +1,24 @@
+import java.math.*;
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        int [][] dp = new int[M + 1][N + 1];
+
+        for (int i = 1 ; i <= M ; i++) {
+            dp[i][0] = 1;
+            for (int j = 1 ; j <= N ; j++) {
+                dp[i][j] = (dp[i][j - 1] + dp[i - 1][j]) % 1000000000;
+            }
+        }
+
+        System.out.println(dp[M][N]);
+    }
+}
