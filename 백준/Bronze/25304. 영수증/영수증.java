772744a88a8eeb12b08613280b549986570ba791
jejinmyeong <1995jjm@naver.com>
2023-05-13 17:44:21 +0900

    [Bronze V] Title: 영수증, Time: 80 ms, Memory: 11588 KB -BaekjoonHub

---
+++ 백준/Bronze/25304. 영수증/영수증.java
@@ -0,0 +1,27 @@
+import java.math.*;
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        int X = Integer.parseInt(br.readLine());
+
+        int N = Integer.parseInt(br.readLine());
+        int sum = 0;
+
+        for (int i = 0 ; i < N ; i++) {
+            st = new StringTokenizer(br.readLine());
+
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+
+            sum += a * b;
+        }
+
+        System.out.println(sum == X ? "Yes" : "No");
+
+    }
+}
