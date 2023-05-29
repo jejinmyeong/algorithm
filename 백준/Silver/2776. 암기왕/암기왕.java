774abf20e5844da5c48b3fc9a01c0e9af5aae3b9
jejinmyeong <1995jjm@naver.com>
2023-05-29 18:56:31 +0900

    [Silver IV] Title: 암기왕, Time: 1796 ms, Memory: 385524 KB -BaekjoonHub

---
+++ 백준/Silver/2776. 암기왕/암기왕.java
@@ -0,0 +1,35 @@
+import java.io.*;
+import java.util.*;
+import java.math.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        StringBuilder sb = new StringBuilder();
+
+        int T = Integer.parseInt(br.readLine());
+
+        while (T-- > 0) {
+            int N = Integer.parseInt(br.readLine());
+
+            HashSet<Integer> set = new HashSet<>();
+
+            st = new StringTokenizer(br.readLine());
+
+            for (int i = 0 ; i < N ; i++) {
+                set.add(Integer.parseInt(st.nextToken()));
+            }
+
+            int M = Integer.parseInt(br.readLine());
+
+            st = new StringTokenizer(br.readLine());
+
+            for (int i = 0 ; i < M ; i++) {
+                if (set.contains(Integer.parseInt(st.nextToken()))) sb.append(1).append("\n");
+                else sb.append(0).append("\n");
+            }
+        }
+        System.out.print(sb);
+    }
+}
