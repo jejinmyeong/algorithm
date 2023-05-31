b2786e80587b5736d35f2a7ee8b93d95ef92f4ab
jejinmyeong <1995jjm@naver.com>
2023-05-31 19:05:21 +0900

    [Silver V] Title: 돌 게임, Time: 88 ms, Memory: 11524 KB -BaekjoonHub

---
+++ 백준/Silver/9655. 돌 게임/돌 게임.java
@@ -0,0 +1,13 @@
+import java.util.*;
+import java.io.*;
+import java.math.*;
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        
+        int N = Integer.parseInt(br.readLine());
+
+        System.out.println(N % 2 == 0 ? "CY" : "SK");
+    }
+}
