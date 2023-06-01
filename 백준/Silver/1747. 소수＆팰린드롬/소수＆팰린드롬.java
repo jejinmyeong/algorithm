c4f5a1f94698a086720f184057fd4ba813ed93ed
jejinmyeong <1995jjm@naver.com>
2023-06-01 15:45:43 +0900

    [Silver I] Title: 소수&팰린드롬, Time: 1012 ms, Memory: 26120 KB -BaekjoonHub

---
+++ 백준/Silver/1747. 소수＆팰린드롬/소수＆팰린드롬.java
@@ -0,0 +1,41 @@
+import java.util.*;
+import java.io.*;
+import java.math.*;
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        int N = Integer.parseInt(br.readLine());
+
+        boolean [] prime = new boolean[10_000_001];
+
+        Arrays.fill(prime, true);
+
+        prime[0] = false;
+        prime[1] = false;
+
+        for (int i = 2 ; i <= 10_000_000 ; i++) {
+            for (int j = i * 2 ; j <= 10_000_000 ; j += i) {
+                prime[j] = false;
+            }
+        }
+
+        for (int i = N ; i <= 10_000_000 ; i++) {
+            if (!prime[i]) continue;
+            String num = String.valueOf(i);
+            boolean flag = true;
+            for (int j = 0 ; j < num.length() / 2 ; j++) {
+                if (num.charAt(j) != num.charAt(num.length() - 1 - j)) {
+                    flag = false;
+                    break;
+                }
+            }
+
+            if (flag) {
+                System.out.println(num);
+                return;
+            }
+        }
+    }
+}
