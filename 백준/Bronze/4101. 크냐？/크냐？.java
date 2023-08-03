78ef18e9f7ea5c77efa35390a434cd98502ead5f
jejinmyeong <1995jjm@naver.com>
2023-08-03 23:05:19 +0900

    [Bronze V] Title: 크냐?, Time: 132 ms, Memory: 14144 KB -BaekjoonHub

---
+++ 백준/Bronze/4101. 크냐？/크냐？.java
@@ -0,0 +1,24 @@
+import java.util.*;
+import java.io.*;
+import java.math.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuffer sb = new StringBuffer();
+
+        while(true) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+
+            if (a == 0 && b == 0) break;
+
+            if (a > b) sb.append("Yes").append("\n");
+            else sb.append("No").append("\n");
+        }
+
+        System.out.print(sb);
+    }
+}
