4787f1d306e962d8450a1215be9139570df0cb65
jejinmyeong <1995jjm@naver.com>
2023-09-02 22:54:00 +0900

    [Bronze V] Title: 이상한 기호, Time: 124 ms, Memory: 14232 KB -BaekjoonHub

---
+++ 백준/Bronze/15964. 이상한 기호/이상한 기호.java
@@ -0,0 +1,17 @@
+import java.util.*;
+import java.io.*;
+import java.math.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        long A = Integer.parseInt(st.nextToken());
+        long B = Integer.parseInt(st.nextToken());
+
+        long res = (A + B) * (A - B);
+
+        System.out.println(res);
+    }
+}
