3aecccbce18b3688954325fcabf6c69dda5fe63f
jejinmyeong <1995jjm@naver.com>
2023-10-14 13:06:11 +0900

    [Bronze III] Title: A+B - 6, Time: 128 ms, Memory: 14300 KB -BaekjoonHub

---
+++ 백준/Bronze/10953. A＋B － 6/A＋B － 6.java
@@ -0,0 +1,24 @@
+import java.io.*;
+import java.util.*;
+import java.math.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        StringBuffer sb = new StringBuffer();
+
+        int T = Integer.parseInt(br.readLine());
+
+        while (T-- > 0) {
+            st = new StringTokenizer(br.readLine(), ",");
+
+            int A = Integer.parseInt(st.nextToken());
+            int B = Integer.parseInt(st.nextToken());
+
+            sb.append(A + B).append("\n");
+        }
+
+        System.out.print(sb);
+    }
+}
