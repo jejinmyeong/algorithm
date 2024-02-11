84fd16d8820ea91d880e5320a10d61bff1bbe217
jejinmyeong <1995jjm@naver.com>
2024-02-11 21:57:33 +0900

    [Silver II] Title: 돌 게임 4, Time: 124 ms, Memory: 14276 KB -BaekjoonHub

---
+++ 백준/Silver/9658. 돌 게임 4/돌 게임 4.java
@@ -0,0 +1,14 @@
+import java.util.*;
+import java.io.*;
+import java.math.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+
+        if (N % 7 == 1 || N % 7 == 3) System.out.println("CY");
+        else System.out.println("SK");
+    }
+}
