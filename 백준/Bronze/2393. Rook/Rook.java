429461c1b891aa943244d550ed58bb5c82d55dfc
jejinmyeong <1995jjm@naver.com>
2024-04-05 21:09:55 +0900

    [Bronze V] Title: Rook, Time: 116 ms, Memory: 13960 KB -BaekjoonHub

---
+++ 백준/Bronze/2393. Rook/Rook.java
@@ -0,0 +1,26 @@
+import java.io.*;
+import java.util.*;
+import java.math.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        sb.append("  ___  ___  ___").append("\n");
+        sb.append("  | |__| |__| |").append("\n");
+        sb.append("  |           |").append("\n");
+        sb.append("   \\_________/").append("\n");
+        sb.append("    \\_______/").append("\n");
+        sb.append("     |     |").append("\n");
+        sb.append("     |     |").append("\n");
+        sb.append("     |     |").append("\n");
+        sb.append("     |     |").append("\n");
+        sb.append("     |_____|").append("\n");
+        sb.append("  __/       \\__").append("\n");
+        sb.append(" /             \\").append("\n");
+        sb.append("/_______________\\").append("\n");
+
+        System.out.println(sb);
+    }
+}
