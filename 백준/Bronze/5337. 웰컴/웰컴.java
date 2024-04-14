909dfe7661e91b65fdf5e9c2217f4723d4fd81ec
jejinmyeong <1995jjm@naver.com>
2024-04-14 23:49:56 +0900

    [Bronze V] Title: 웰컴, Time: 108 ms, Memory: 13904 KB -BaekjoonHub

---
+++ 백준/Bronze/5337. 웰컴/웰컴.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.util.*;
+import java.math.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        sb.append(".  .   .").append("\n");
+        sb.append("|  | _ | _. _ ._ _  _").append("\n");
+        sb.append("|/\\|(/.|(_.(_)[ | )(/.");
+
+        System.out.println(sb);
+
+    }
+}
