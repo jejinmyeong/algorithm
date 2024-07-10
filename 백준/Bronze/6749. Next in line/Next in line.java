65ad85d18aaca3e3e5c9f448fa5041101c53ca8b
jejinmyeong <1995jjm@naver.com>
2024-07-10 19:12:00 +0900

    [Bronze IV] Title: Next in line, Time: 120 ms, Memory: 13984 KB -BaekjoonHub

---
+++ 백준/Bronze/6749. Next in line/Next in line.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.util.*;
+import java.math.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int first = Integer.parseInt(br.readLine());
+        int second = Integer.parseInt(br.readLine());
+        int last = second + second - first;
+
+        System.out.println(last);
+    }
+}
