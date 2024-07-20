35d558a62c612f3180bf9a378a5b860d3ee5c421
jejinmyeong <1995jjm@naver.com>
2024-07-20 20:35:15 +0900

    [Bronze IV] Title: 한글, Time: 124 ms, Memory: 14224 KB -BaekjoonHub

---
+++ 백준/Bronze/11282. 한글/한글.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.util.*;
+import java.math.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+
+        N += 44031;
+
+        char c = (char) N;
+
+        System.out.println(c);
+    }
+}
