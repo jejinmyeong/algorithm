26636c50bedb74bc2c0e4ba1768bdd9e1b637977
jejinmyeong <1995jjm@naver.com>
2024-07-28 22:40:40 +0900

    [Bronze IV] Title: FYI, Time: 124 ms, Memory: 14176 KB -BaekjoonHub

---
+++ 백준/Bronze/17863. FYI/FYI.java
@@ -0,0 +1,11 @@
+import java.io.*;
+import java.util.*;
+import java.math.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        System.out.println(br.readLine().indexOf("555") == 0 ? "YES" : "NO");
+    }
+}
