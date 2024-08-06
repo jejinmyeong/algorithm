660fee58546c170f7ff48963ce5bec48b81b4db9
jejinmyeong <1995jjm@naver.com>
2024-08-06 22:39:47 +0900

    [Bronze IV] Title: Abbey Courtyard, Time: 104 ms, Memory: 14156 KB -BaekjoonHub

---
+++ 백준/Bronze/15610. Abbey Courtyard/Abbey Courtyard.java
@@ -0,0 +1,11 @@
+import java.io.*;
+import java.util.*;
+import java.math.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        System.out.println(Math.sqrt(Long.parseLong(br.readLine())) * 4);
+    }
+}
