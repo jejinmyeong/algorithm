c26339c536608619a8b3df0d2d6180349d63464f
jejinmyeong <1995jjm@naver.com>
2024-08-29 15:09:30 +0900

    [Bronze IV] Title: Easy-to-Solve Expressions, Time: 108 ms, Memory: 14224 KB -BaekjoonHub

---
+++ 백준/Bronze/25784. Easy－to－Solve Expressions/Easy－to－Solve Expressions.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.util.*;
+import java.math.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+        int c = Integer.parseInt(st.nextToken());
+        
+        if (a + b == c || a + c == b || b + c == a) {
+            System.out.println(1);
+        } else if (a * b == c || a * c == b | b * c == a) {
+            System.out.println(2);
+        } else {
+            System.out.println(3);
+        }
+    }
+}
