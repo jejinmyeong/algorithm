aee9dc10029654384488c05de6e98de35a60883c
jejinmyeong <1995jjm@naver.com>
2024-08-31 03:14:43 +0900

    [Bronze IV] Title: Population, Time: 96 ms, Memory: 13988 KB -BaekjoonHub

---
+++ 백준/Bronze/26561. Population/Population.java
@@ -0,0 +1,25 @@
+import java.io.*;
+import java.util.*;
+import java.math.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        StringBuilder sb = new StringBuilder();
+
+        int N = Integer.parseInt(br.readLine());
+
+        while (N-- > 0) {
+            st = new StringTokenizer(br.readLine());
+
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+
+            int t = a + b / 4 - b / 7;
+            sb.append(t).append("\n");
+        }
+
+        System.out.println(sb);
+    }
+}
