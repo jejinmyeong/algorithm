3a976f726160443100071c64fd50041b759b6f25
jejinmyeong <1995jjm@naver.com>
2024-09-15 03:20:52 +0900

    [Bronze IV] Title: SciComLove (2024), Time: 100 ms, Memory: 14104 KB, Score: 100 point -BaekjoonHub

---
+++ 백준/Bronze/31746. SciComLove （2024）/SciComLove （2024）.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.util.*;
+import java.math.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+
+        if (n % 2 == 0) {
+            System.out.println("SciComLove");
+        } else {
+            System.out.println("evoLmoCicS");
+        }
+    }
+}
