2253ad560cd1244ce173ff73b041ab6fca6ef4a4
jejinmyeong <1995jjm@naver.com>
2024-09-18 23:54:52 +0900

    [Bronze IV] Title: Лифт, Time: 124 ms, Memory: 15984 KB -BaekjoonHub

---
+++ 백준/Bronze/27262. Лифт/Лифт.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.util.*;
+import java.math.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+
+        int res1 = (k + n - 2) * b;
+        int res2 = (n - 1) * a;
+
+        System.out.println(res1 + " " + res2);
+    }
+}
