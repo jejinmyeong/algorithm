a5aa5c9d3dc5c5413447558e71680005686f00ea
jejinmyeong <1995jjm@naver.com>
2024-09-25 23:08:34 +0900

    [Bronze IV] Title: 한글 2, Time: 104 ms, Memory: 14244 KB -BaekjoonHub

---
+++ 백준/Bronze/11283. 한글 2/한글 2.java
@@ -0,0 +1,13 @@
+import java.io.*;
+import java.util.*;
+import java.math.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        char c = br.readLine().charAt(0);
+
+        System.out.println((int) c - 44031);
+    }
+}
