7d6627c4dfc68eca0d8fd3dac5a7a018c76e821c
jejinmyeong <1995jjm@naver.com>
2024-10-09 16:52:30 +0900

    [Bronze IV] Title: Celebrity jeopardy, Time: 96 ms, Memory: 14000 KB -BaekjoonHub

---
+++ 백준/Bronze/3765. Celebrity jeopardy/Celebrity jeopardy.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.*;
+import java.math.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        while (true) {
+            String s = br.readLine();
+            if (s == null) break;
+            sb.append(s).append('\n');
+        }
+        System.out.print(sb);
+    }
+}
