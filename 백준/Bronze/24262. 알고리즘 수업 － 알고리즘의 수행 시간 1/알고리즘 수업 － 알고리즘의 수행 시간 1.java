7569c8c204ef0b7e781d058b563fb9a34251e9df
jejinmyeong <1995jjm@naver.com>
2024-11-14 14:15:38 +0900

    [Bronze V] Title: 알고리즘 수업 - 알고리즘의 수행 시간 1, Time: 100 ms, Memory: 14048 KB -BaekjoonHub

---
+++ 백준/Bronze/24262. 알고리즘 수업 － 알고리즘의 수행 시간 1/알고리즘 수업 － 알고리즘의 수행 시간 1.java
@@ -0,0 +1,10 @@
+import java.io.*;
+import java.util.*;
+import java.math.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        System.out.println(1);
+        System.out.println(0);
+    }
+}
