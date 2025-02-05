f865eae8adcccd582aae7353cd705c4be600e081
jejinmyeong <1995jjm@naver.com>
2025-02-05 23:10:13 +0900

    [Silver II] Title: 초콜릿 식사, Time: 100 ms, Memory: 14096 KB -BaekjoonHub

--- 백준/Silver/2885. 초콜릿 식사/초콜릿 식사.java
+++ 백준/Silver/2885. 초콜릿 식사/초콜릿 식사.java
@@ -4,32 +4,29 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
 
         int K = Integer.parseInt(br.readLine());
 
-        int N = 1;
+        int size = 1;
 
-        int [] dp = new int[(int) Math.pow(2, 20) + 1];
+        while (size < K) size *= 2;
 
-        for (int i = 1 ; i < 20 ; i++) {
-            for (int j = (int) Math.pow(2, i) + 1 ; j < (int) Math.pow(2, i + 1) ; j += 2){
-                dp[j] = i + 1;
+        sb.append(size).append(" ");
+
+        int count = 0;
+
+        while (K > 0) {
+            if (K >= size) {
+                K -= size;
+            } else {
+                size /= 2;
+                count++;
             }
         }
 
-        for (int i = 4 ; i < (int) Math.pow(2, 20) + 1 ; i++) {
-            int temp = i;
-            while (temp % 2 == 0) {
-                temp /= 2;
-            }
+        sb.append(count);
 
-            dp[i] = dp[temp];
-        }
-
-        while (N < K) {
-            N *= 2;
-        }
-
-        System.out.println(N + " " + dp[K]);
+        System.out.println(sb);
     }
 }
