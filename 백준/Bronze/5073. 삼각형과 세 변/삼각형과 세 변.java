61bfbeaed85d6c3c620d564d16749b21255182bf
JE JIN MYEONG <60130075+jejinmyeong@users.noreply.github.com>
2025-03-13 03:59:24 +0900

    [Bronze III] Title: 삼각형과 세 변, Time: 100 ms, Memory: 14008 KB -BaekjoonHub

--- 백준/Bronze/5073. 삼각형과 세 변/삼각형과 세 변.java
+++ 백준/Bronze/5073. 삼각형과 세 변/삼각형과 세 변.java
@@ -9,16 +9,14 @@ public class Main {
 
         while (true) {
             st = new StringTokenizer(br.readLine());
-
+            
             int a = Integer.parseInt(st.nextToken()), b = Integer.parseInt(st.nextToken()), c = Integer.parseInt(st.nextToken());
 
             if (a == 0 && b == 0 && c == 0) {
                 break;
             }
 
-            int max = Math.max(a, Math.max(b, c));
-
-            if (a + b + c <= max * 2) {
+            if (a + b <= c || b + c <= a || a + c <= b) {
                 sb.append("Invalid\n");
             } else if (a == b && b == c) {
                 sb.append("Equilateral\n");
