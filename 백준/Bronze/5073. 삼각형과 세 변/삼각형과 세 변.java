11c6c1a03b9d87675603e92064c8ee5b9625c1de
JE JIN MYEONG <60130075+jejinmyeong@users.noreply.github.com>
2025-03-13 03:59:01 +0900

    [Bronze III] Title: 삼각형과 세 변, Time: 100 ms, Memory: 14060 KB -BaekjoonHub

---
+++ 백준/Bronze/5073. 삼각형과 세 변/삼각형과 세 변.java
@@ -0,0 +1,36 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        StringBuilder sb = new StringBuilder();
+
+        while (true) {
+            st = new StringTokenizer(br.readLine());
+
+            int a = Integer.parseInt(st.nextToken()), b = Integer.parseInt(st.nextToken()), c = Integer.parseInt(st.nextToken());
+
+            if (a == 0 && b == 0 && c == 0) {
+                break;
+            }
+
+            int max = Math.max(a, Math.max(b, c));
+
+            if (a + b + c <= max * 2) {
+                sb.append("Invalid\n");
+            } else if (a == b && b == c) {
+                sb.append("Equilateral\n");
+            } else if (a == b || a == c || b == c) {
+                sb.append("Isosceles\n");
+            } else {
+                sb.append("Scalene\n");
+            }
+        }
+
+        System.out.print(sb);
+
+    }
+}
+
