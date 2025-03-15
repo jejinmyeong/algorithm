6b0d782455306d2d5646b11042736541ddd8aa50
JE JIN MYEONG <60130075+jejinmyeong@users.noreply.github.com>
2025-03-16 03:49:24 +0900

    [Bronze V] Title: The World Responds, Time: 100 ms, Memory: 14036 KB -BaekjoonHub

---
+++ 백준/Bronze/23234. The World Responds/The World Responds.java
@@ -0,0 +1,8 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        System.out.println("The world says hello!");
+    }
+}
