c1f74edfc019b53c0b026fec271fd57cac2d42c1
JE JIN MYEONG <60130075+jejinmyeong@users.noreply.github.com>
2025-03-21 00:19:42 +0900

    [Silver II] Title: 가장 큰 증가하는 부분 수열, Time: 160 ms, Memory: 9912 KB -BaekjoonHub

---
+++ 백준/Silver/11055. 가장 큰 증가하는 부분 수열/가장 큰 증가하는 부분 수열.js
@@ -0,0 +1,23 @@
+const fs = require('fs');
+const input = fs.readFileSync('/dev/stdin').toString().split('\n');
+
+function main() {
+    const N = input[0];
+    const arr = input[1].split(' ').map(Number);
+
+    const dp = [];
+
+    for (let i = 0 ; i < N ; i++) {
+        dp[i] = arr[i];
+    }
+
+    for (let i = 1 ; i < N ; i++) {
+        for (let j = 0 ; j < i ; j++) {
+            if (arr[j] < arr[i]) dp[i] = Math.max(dp[i], dp[j] + arr[i]);
+        }
+    }
+
+    console.log(Math.max(...dp));
+}
+
+main();
