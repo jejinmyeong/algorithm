8fc348f1a0470201d72db9a65f417c3e3577d5bb
JE JIN MYEONG <60130075+jejinmyeong@users.noreply.github.com>
2025-06-24 23:34:29 +0900

    [Bronze V] Title: Арифметическая магия, Time: 96 ms, Memory: 9332 KB -BaekjoonHub

---
+++ 백준/Bronze/18096. Арифметическая магия/Арифметическая магия.js
@@ -0,0 +1,14 @@
+const fs = require("fs");
+const input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
+
+let currentLine = 0;
+
+const readLine = () => {
+  return input[currentLine++];
+}
+
+const main = () => {
+  console.log(1)
+}
+
+main();
