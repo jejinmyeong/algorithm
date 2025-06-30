9e22c48df51cb18ea743d7487caee5817a7ac5a0
JE JIN MYEONG <60130075+jejinmyeong@users.noreply.github.com>
2025-06-30 22:25:12 +0900

    [Bronze I] Title: 세준세비, Time: 368 ms, Memory: 35300 KB -BaekjoonHub

--- 백준/Bronze/1524. 세준세비/세준세비.js
+++ 백준/Bronze/1524. 세준세비/세준세비.js
@@ -17,23 +17,7 @@ const main = () => {
     const sejun = readLine().split(' ').map(Number).sort((a, b) => b - a);
     const sebi = readLine().split(' ').map(Number).sort((a, b) => b - a);
 
-    while (sejun.length + sebi.length !== 1) {
-      if (sejun.length === 0) {
-        sebi.pop();
-        continue;
-      }
-
-      if (sebi.length === 0) {
-        sejun.pop();
-        continue;
-      }
-
-      if (sejun.at(-1) >= sebi.at(-1)) sebi.pop();
-      else sejun.pop();
-    }
-
-    if (sejun.length > sebi.length) return 'S';
-    else return 'B';
+    return sejun[0] >= sebi[0] ? 'S' : 'B'
   })
 
   console.log(res.join('\n'))
