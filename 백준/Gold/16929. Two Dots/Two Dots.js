4f1480321ee6c6da68e6f75a1351f16295de9a9b
JE JIN MYEONG <60130075+jejinmyeong@users.noreply.github.com>
2025-07-01 18:20:39 +0900

    [Gold IV] Title: Two Dots, Time: 152 ms, Memory: 10184 KB -BaekjoonHub

--- 백준/Gold/16929. Two Dots/Two Dots.js
+++ 백준/Gold/16929. Two Dots/Two Dots.js
@@ -7,48 +7,47 @@ const readLine = () => {
   return input[currentLine++];
 }
 
-const D = [[ -1, 0 ], [ 0, 1 ], [ 1, 0 ], [ 0, -1]];
-let N, M;
-let map;
-let visited;
-let curX, curY;
-
-const dfs = (x, y, cnt) => {
-  for (let d = 0 ; d < 4 ; d++) {
-    const nx = x + D[d][0];
-    const ny = y + D[d][1];
-
-    if (nx < 0 || ny < 0 || nx >= N || ny >= M || map[nx][ny] !== map[x][y]) continue;
-
-    if (visited[nx][ny]) {
-      if (cnt >= 4 && curX === nx && curY === ny) {
-        console.log('Yes');
-        process.exit(0);
-      }
-    } else {
-      visited[nx][ny] = true;
-      dfs(nx, ny, cnt + 1);
-    }
-  }
-}
-
 const main = () => {
-  [ N, M ] = readLine().split(' ').map(Number);
-
-  map = Array(N).fill(null);
-
+  const [ N, M ] = readLine().split(' ').map(Number);
+  
+  const map = Array(N).fill(null);
+  const visited = Array.from({ length: N }, () => Array(M).fill(false));
+  
   for (let i = 0 ; i < N ; i++) {
     map[i] = readLine().split('');
+  }
+  
+  const D = [[ -1, 0 ], [ 0, 1 ], [ 1, 0 ], [ 0, -1 ]]; 
+  
+  const dfs = (x, y, pd) => {
+    for (let d = 0 ; d < 4 ; d++) {
+      if ((d + 2) % 4 === pd) continue;
+
+      const nx = x + D[d][0];
+      const ny = y + D[d][1];
+      
+      if (nx < 0 || ny < 0 || nx >= N || ny >= M || map[nx][ny] !== map[x][y]) continue;
+      
+      if (visited[nx][ny]) {
+        return true;
+      } else {
+        visited[nx][ny] = true;
+        if (dfs(nx, ny, d)) return true;
+      }
+    }
+
+    return false;
   }
 
   for (let i = 0 ; i < N ; i++) {
     for (let j = 0 ; j < M ; j++) {
-      visited = Array.from({ length: N }, () => Array(M).fill(false));
+      if (visited[i][j]) continue;
       visited[i][j] = true;
-      curX = i;
-      curY = j;
 
-      dfs(i, j, 1);
+      if (dfs(i, j, -1)) {
+        console.log("Yes");
+        return;
+      }
     }
   }
   
