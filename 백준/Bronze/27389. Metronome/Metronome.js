6c1dcb825cd9245c21cfd98f2a623fa5ced6c37e
JE JIN MYEONG <60130075+jejinmyeong@users.noreply.github.com>
2025-07-08 23:05:03 +0900

    [Bronze V] Title: Metronome, Time: 92 ms, Memory: 9332 KB -BaekjoonHub

---
+++ 백준/Bronze/27389. Metronome/Metronome.js
@@ -0,0 +1,16 @@
+const fs = require("fs");
+const input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
+
+let currentLine = 0;
+
+const readLine = () => {
+  return input[currentLine++];
+}
+
+const main = () => {
+  const N = +readLine();
+
+  console.log((N / 4).toFixed(2))
+}
+
+main();
